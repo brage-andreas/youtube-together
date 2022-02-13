9bf2ea0d7a6f9b6402dfe4403949203bb54831a3
Drango <brage.spammail@gmail.com>
2022-02-14 00:00:07 +0100

    feat: modularity

---
+++ src/activities.ts
@@ -0,0 +1,20 @@
+// https://gist.github.com/GeneralSadaf/42d91a2b6a93a7db7a39208f2d8b53ad
+
+interface Activity {
+	commandName: string;
+	name: string;
+	id: string;
+}
+
+export const activities: Activity[] = [
+	{
+		commandName: "youtube",
+		name: "YouTube Watch Together",
+		id: "880218394199220334"
+	}
+	/*{
+		commandName: "sketch-heads",
+		name: "Sketch Heads",
+		id: "902271654783242291"
+	}*/
+];
