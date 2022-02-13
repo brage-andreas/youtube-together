8041d16d2996ad2202929604ad2bf385a3bffc60
Drango <brage.spammail@gmail.com>
2022-02-14 00:01:12 +0100

    feat: better error handling, audit logs, and modularity

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 import { Client, IntentsBitField } from "discord.js";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import { activities } from "./activities.js";
 dotenv.config();
 const { BOT_TOKEN, BOT_ID } = process.env;
 if (!BOT_TOKEN || !BOT_ID)
@@ -40,32 +41,40 @@ client.on("ready", () => {
     console.log(`In ${guilds} guilds with ${users} total members`);
 });
 client.on("interactionCreate", async (intr) => {
-    if (!intr.isChatInputCommand() || intr.commandName !== "youtube")
+    const getStr = (name) => `${/^[aeiou]$/gi.test(name.trim().charAt(0)) ? "an" : "a"} ${name}`;
+    if (!intr.isChatInputCommand())
         return;
+    await intr.deferReply();
     if (!intr.guild)
-        return intr.reply({ content: "This command can only be used inside a server.", ephemeral: true });
+        return void intr.editReply({ content: "This command can only be used inside a server." });
     if (!intr.inCachedGuild())
-        return intr.reply({ content: "Something went wrong with retrieving this server.", ephemeral: true });
+        return void intr.editReply({ content: "Something went wrong with retrieving this server." });
     const { channelId } = intr.member.voice;
     if (!channelId)
-        return intr.reply({ content: "You need to be in a voice channel.", ephemeral: true });
-    await intr.deferReply();
+        return void intr.editReply({ content: "You need to be in a voice channel." });
+    const activity = activities.find((a) => a.commandName === intr.commandName);
+    if (!activity)
+        return void intr.editReply({
+            content: "Some of my gears broke — I cannot find the activity for this command"
+        });
     const url = `https://discord.com/api/v9/channels/${channelId}/invites`;
     const headers = {
+        "X-Audit-Log-Reason": `Launched event | Run by ${intr.user.tag} (${intr.user.id})`,
         "Content-Type": "application/json",
         Authorization: `Bot ${BOT_TOKEN}`
     };
     const body = JSON.stringify({
-        target_application_id: "880218394199220334",
+        target_application_id: activity.id,
         target_type: 2
     });
     const invite = (await fetch(url, { method: "POST", headers, body })
         .then((res) => res.json())
-        .catch(() => null));
+        .catch(console.log));
     if (!invite?.code)
         return void intr.editReply("Something went wrong with creating the invite.");
-    if (invite.code === "50013")
-        return void intr.editReply("I am missing permissions to launch activities or create invites.");
-    intr.editReply(`Successfully made a Watch Together activity!\n[Click here to join.](https://discord.com/invite/${invite.code})`);
+    if (invite.code.toString().startsWith("500"))
+        return void intr.editReply("Something went wrong. I am likely missing permissions to create this invite.");
+    const link = `[Click here to join.](https://discord.com/invite/${invite.code})`;
+    intr.editReply(`Successfully made ${getStr(activity.name)} activity!\n${link}`);
 });
 client.login(BOT_TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Client, IntentsBitField, Invite } from "discord.js";
+import { Client, IntentsBitField } from "discord.js";
+import { APIInvite } from "discord.js/node_modules/discord-api-types";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import { activities } from "./activities.js";
 
 dotenv.config();
 
@@ -47,42 +49,53 @@ client.on("ready", () => {
 });
 
 client.on("interactionCreate", async (intr) => {
-	if (!intr.isChatInputCommand() || intr.commandName !== "youtube") return;
+	const getStr = (name: string) => `${/^[aeiou]$/gi.test(name.trim().charAt(0)) ? "an" : "a"} ${name}`;
 
-	if (!intr.guild) return intr.reply({ content: "This command can only be used inside a server.", ephemeral: true });
+	if (!intr.isChatInputCommand()) return;
+
+	await intr.deferReply();
+
+	if (!intr.guild) return void intr.editReply({ content: "This command can only be used inside a server." });
 
 	if (!intr.inCachedGuild())
-		return intr.reply({ content: "Something went wrong with retrieving this server.", ephemeral: true });
+		return void intr.editReply({ content: "Something went wrong with retrieving this server." });
 
 	const { channelId } = intr.member.voice;
 
-	if (!channelId) return intr.reply({ content: "You need to be in a voice channel.", ephemeral: true });
+	if (!channelId) return void intr.editReply({ content: "You need to be in a voice channel." });
 
-	await intr.deferReply();
+	const activity = activities.find((a) => a.commandName === intr.commandName);
+
+	if (!activity)
+		return void intr.editReply({
+			content: "Some of my gears broke — I cannot find the activity for this command"
+		});
 
 	const url = `https://discord.com/api/v9/channels/${channelId}/invites`;
 
 	const headers = {
+		"X-Audit-Log-Reason": `Launched event | Run by ${intr.user.tag} (${intr.user.id})`,
 		"Content-Type": "application/json",
 		Authorization: `Bot ${BOT_TOKEN}`
 	};
 
 	const body = JSON.stringify({
-		target_application_id: "880218394199220334",
+		target_application_id: activity.id,
 		target_type: 2
 	});
 
 	const invite = (await fetch(url, { method: "POST", headers, body })
 		.then((res) => res.json())
-		.catch(() => null)) as Invite | null;
+		.catch(() => null)) as APIInvite | null;
 
 	if (!invite?.code) return void intr.editReply("Something went wrong with creating the invite.");
 
-	if (invite.code === "50013") return void intr.editReply("I am missing permissions to create invites.");
+	if (invite.code.toString().startsWith("500"))
+		return void intr.editReply("Something went wrong. I am likely missing permissions to create this invite.");
 
-	intr.editReply(
-		`Successfully made a Watch Together activity!\n[Click here to join.](https://discord.com/invite/${invite.code})`
-	);
+	const link = `[Click here to join.](https://discord.com/invite/${invite.code})`;
+
+	intr.editReply(`Successfully made ${getStr(activity.name)} activity!\n${link}`);
 });
 
 client.login(BOT_TOKEN);
